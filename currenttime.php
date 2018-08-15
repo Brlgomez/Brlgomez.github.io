a09bf03ed0ad9154872f53787fbfa64140bd90db
Brandon Gomez <brlgomez@ucsc.edu>
2018-08-15 12:27:38 -0700

    Update

--- currenttime.php
+++ currenttime.php
@@ -1,6 +1,14 @@
-<?php
- date_default_timezone_set('US/Eastern');
- $currenttime = date("m-d-Y H:i:s");
- list($ddd,$ttt) = split(' ',$currenttime);
- echo "$ddd/$ttt\n";
-?
+<html>
+	<head>
+ 		<title>PHP Test</title>
+	</head>
+	<body>
+		<?php
+ 		date_default_timezone_set('US/Eastern');
+ 		$currenttime = date("m-d-Y H:i:s");
+ 		list($ddd,$ttt) = split(' ',$currenttime);
+ 		echo "$ddd/$ttt\n";
+		?
+	</body>
+</html>
+
