af7241289932f3208fd28ee35b13efafbd28d105
Brandon Gomez <brlgomez@ucsc.edu>
2018-08-15 12:35:03 -0700

    Retrying this way

--- currenttime.php
+++ currenttime.php
@@ -1,14 +1,6 @@
-<html>
-	<head>
- 		<title>PHP Test</title>
-	</head>
-	<body>
-		<?php
- 		date_default_timezone_set('US/Eastern');
- 		$currenttime = date("m-d-Y H:i:s");
- 		list($ddd,$ttt) = split(' ',$currenttime);
- 		echo "$ddd/$ttt\n";
-		?
-	</body>
-</html>
-
+<?php
+ date_default_timezone_set('US/Eastern');
+ $currenttime = date("m-d-Y H:i:s");
+ list($ddd,$ttt) = split(' ',$currenttime);
+ echo "$ddd/$ttt\n";
+?
